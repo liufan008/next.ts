a7fcbc6bb67c11327fb5d9894d6d1557b3c7eaf2
henry <tarou.88@gmail.com>
2019-12-03 20:35:28 +0900

    🚀Apollo GraphQL  & inMemoryCache
    [chore] Add Provider to to _App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { AppContext, AppInitialProps } from "next/app";
 import Link from "next/link";
 import NProgress from "nprogress";
 import Router from "next/router";
 import Head from "next/head";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import withData from "~/utils/withApollo";
+import App from "next/app";
 
 Router.events.on("routeChangeStart", url => {
   console.log(`Loading: ${url}`);
@@ -12,32 +15,40 @@ Router.events.on("routeChangeStart", url => {
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-const App = ({
-  Component,
-  pageProps
-}: AppContext & AppInitialProps): JSX.Element => (
-  <>
-    <Head>
-      <link rel="stylesheet" type="text/css" href="/nprogress.css" />
-    </Head>
-    <nav>
-      <style jsx>{`
-        a {
-          margin: 0 10px 0 0;
-        }
-      `}</style>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/stars" as="github-stars">
-        <a>Stars</a>
-      </Link>
-    </nav>
-    <Component {...pageProps} />
-  </>
-);
+interface Props {
+  apollo: ApolloClient<{}>;
+}
 
-export default App;
+class MyApp extends App<Props> {
+  render(): JSX.Element {
+    const { Component, pageProps, apollo } = this.props;
+    return (
+      <>
+        <ApolloProvider client={apollo}>
+          <Head>
+            <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+          </Head>
+          <nav>
+            <style jsx>{`
+              a {
+                margin: 0 10px 0 0;
+              }
+            `}</style>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+            <Link href="/stars" as="github-stars">
+              <a>Stars</a>
+            </Link>
+          </nav>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </>
+    );
+  }
+}
+
+export default withData(MyApp);
