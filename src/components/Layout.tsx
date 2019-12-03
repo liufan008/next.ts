60c98adc0be2fc995fb4a502709eaf4d3b779185
henry <tarou.88@gmail.com>
2019-12-03 11:36:08 +0900

    _App Layout
    🏗️_app.tsx
    ➕nprogress

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,21 +13,6 @@ const Layout: React.FC<Props> = ({ children, title = "Layout Title" }) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/stars" as="github-stars">
-          <a>Stars</a>
-        </Link>{" "}
-      </nav>
-    </header>
     {children}
     <footer>
       <hr />
