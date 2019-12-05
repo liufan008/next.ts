80c5c891cde8f4331f2e5093501d1b2dfbf8bf7d
hype08 <38557414+hype08@users.noreply.github.com>
2019-12-05 09:29:57 +0900

    :heavy_plus_sign: add apollo-link-error

--- src/utils/withApollo.ts
+++ src/utils/withApollo.ts
@@ -1,8 +1,19 @@
 import withApollo from "next-with-apollo";
 import ApolloClient, { InMemoryCache } from "apollo-boost";
+import { onError } from "apollo-link-error";
 
 const GRAPHQL_ENDPOINT_DEVELOPMENT = "http://localhost:4000";
 const GRAPHQL_ENDPOINT_PRODUCTION = "http://ALIAS.USER.now.sh/API_ENDPOINT";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `GraphQL error: Message ${message}, Location: ${locations} Path: ${path}`
+      )
+    )
+  if (networkError) console.log(`Network error: ${networkError}`)
+});
 
 const GRAPHQL_ENDPOINT =
   process.env.NODE_ENV === "development"
